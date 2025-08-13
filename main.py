371b95ce5697efbb1e349e0b2107d448aa3651a9
arthur1312qr1 <marciamabab@gmail.com>
2025-08-13 18:55:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from app import app, socketio
 import os
 
 if __name__ == '__main__':
+    # Usa PORT do ambiente ou 5000 como fallback
     port = int(os.environ.get('PORT', 5000))
+    print(f"Iniciando servidor na porta: {port}")
     socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
