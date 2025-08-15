e07fb6acc34d255625a003b722166160531d3842
arthur1312qr1 <marciamabab@gmail.com>
2025-08-15 19:42:01 -0300

    Update trading_bot.py

--- trading_bot.py
+++ trading_bot.py
@@ -59,7 +59,7 @@ class TradingBot:
         
         # Risk management
         self.stop_loss_pct = 0.02  # 2%
-        self.take_profit_pct = 0.007  # 0.7%
+        self.take_profit_pct = 0.01  # 1%
         self.position_size_pct = 0.1  # 10% of balance per trade
         
         # Activity log
