55a102860bb4613b09a34bcada26936f8cb3ff2f
arthur1312qr1 <marciamabab@gmail.com>
2025-08-14 21:28:05 -0300

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import TradingDashboard from "@/components/TradingDashboard";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      {/* Trading Dashboard Home Page */}
+      <Route path="/" component={TradingDashboard} />
+      
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
